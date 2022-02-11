fae4edc088114f8d3cb428bdd51e89a5e4bf03f0
riddhi <riddhisanghani92@gmail.com>
2022-02-11 16:44:18 +0530

    master

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
 import { Component } from "react/cjs/react.development";
-
+import Menu from "./component/Menu";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-      </div>
+      // <div className="App">
+      //   <Navbar dark color="primary">
+      //     <div className="container">
+      //       <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+      //     </div>
+      //   </Navbar>
+      // </div>
+      <Menu />
     );
   }
 }
