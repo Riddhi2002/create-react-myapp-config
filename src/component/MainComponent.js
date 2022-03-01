46dcc31b3cb2e51c32e06b605674d55257386f64
riddhi <riddhisanghani92@gmail.com>
2022-03-01 16:44:31 +0530

    React Router

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -1,42 +1,37 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./Menu";
 import { DISHES } from "../shared/dishes";
 import DishDetail from "./DishdetailComponent";
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+import Contact from "./ContactComponent";
+import { Route, Switch, Redirect } from "react-router-dom";
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dish: DISHES,
-      selectedDish: null,
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({
-      selectedDish: dishId,
-    });
-  }
-
   render() {
+    const HomePage = () => {
+      return <Home />;
+    };
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/"> Ristorante Con Fusion </NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu
-          dishes={this.state.dish}
-          onClick={(dishId) => this.onDishSelect(dishId)}
-        />
-        <DishDetail
-          dish={
-            this.state.dish.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            path="/menu"
+            component={() => <Menu dishes={this.state.dish} />}
+          />
+          <Route path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap-social/bootstrap-social.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById("root")
 );
 
