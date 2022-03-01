59ff2eae750315c2623db68af54a56c6012bb723
riddhi <riddhisanghani92@gmail.com>
2022-03-01 19:21:15 +0530

    Single page application

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -6,18 +6,31 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Home from "./Home";
 import Contact from "./ContactComponent";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leader";
+import { PROMOTIONS } from "../shared/promotions";
 import { Route, Switch, Redirect } from "react-router-dom";
+
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dish: DISHES,
+      comment: COMMENTS,
+      promotion: PROMOTIONS,
+      leader: LEADERS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dish.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotion.filter((promo) => promo.featured)[0]}
+          leader={this.state.leader.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
     return (
       <div>
